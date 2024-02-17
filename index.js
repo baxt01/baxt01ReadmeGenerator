e85f65941d814e85e136b18d32b9f258768c3b64
Michael Baxter <baxt01@hotmail.com>
2024-02-17 19:08:33 +0000

    I have added a series of inputs and checkbox into the .prompt all with a default option mainly to offer suggestion to the user, In the answers I have deconstructed the obj and temporarly loged some of the answers to consoleor in the case back to the cli

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 // require the path library
 const path = require('path');
 // require the inquirer library
-import inquirer from 'inquirer';
+const inquirer = require('inquirer');
 // require the local file 
 const generateMarkdown = require("./utils/generateMarkdown");
 
@@ -12,22 +12,81 @@ inquirer
     /* Pass your questions in here */
     {
         type: 'input',
-        name: 'input_type',
-        message: 'What is your name?',
-        default: 'Michael'
+        name: 'project_title',
+        message: 'What is your project title?',
+        default: 'repo'
     },
     {
-        type: 'list',
-        name: 'list question',
-        message: 'What programming language do you like?',
-        choices:['Javascript', 'C++', 'python', 'ruby'],
-        default: 'Javascript'
+        type: 'input',
+        name: 'description',
+        message: 'Type a description of you\'re project?',
+        default: 'This is my project repo...'
+    },
+    {
+      type: 'checkbox',
+      name: 'table_of_contents',
+      message: 'Which sections do you wish to include in the table of contents?',
+      choices: ['installation', 'usage', 'license', 'contributing', 'tests', 'questions'],
+      default: 'installation'
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'How can a user install your application?',
+      default: 'Clone the repo to you\'re local drive.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'What can you\'re application be used for?',
+      default: 'working.'
+    },
+    {
+      type: 'input',
+      name: 'license',
+      message: 'Which type of License does your application come with?',
+      default: 'MIT'
+    },
+    {
+      type: 'input',
+      name: 'Contributing',
+      message: 'Who has and who can Contribute to your project',
+      default: 'Open source.'
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'What run time test have been performed on this project?',
+      default: 'I.E Github pages etc,',
+    },
+    {
+      type: 'input',
+      name: 'questions',
+      message: 'where can questions and issues be reported to?',
+      default: 'your github issues section.'
     }
+
+    //need to look at how a user can add screenshots to this app.
+    
   ])
   .then((answers) => {
     // Use user feedback for... whatever!!
-  })
+    const {project_title, description, table_of_contents, installation, usage, license, Contributing, tests, questions} = answers;
+    
+    // console.log(answers);
 
+    // const title = answers.project_title;
+    // const description = answers.Description;
+    // const table_of_contents = answers.table_of_contents;
+
+
+    console.log(project_title);
+    console.log(description);
+    console.log(table_of_contents);
+    
+
+
+  })
 
 //   .catch((error) => {
 //     if (error.isTtyError) {
